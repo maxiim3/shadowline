3cd9bf88e834b9268edcbe0fd2392b8053529487
Maxime <93276593+maxiim3@users.noreply.github.com>
2026-07-20 15:19:08 +0200

    feat: add playable Shadowline V1

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooks.configs.flat['recommended-latest'],
+  reactRefresh.configs.vite,
+)
